b6b9962d311b66eea55e630fa4993dfe99e4375d
Amiruddin Irsyad <amiruddinid@gmail.com>
2023-11-10 22:10:07 +0700

    imagekit + qrcode

--- app/controller/api/v2/media.js
+++ app/controller/api/v2/media.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
+const qr = require('node-qr-image');
+const imagekit = require('../../../../utils/imagekit');
 
 module.exports = {
     uploadImage: async (req, res) => {
@@ -34,5 +36,37 @@ module.exports = {
                 video_url: videoUrl
             }
         })
+    },
+    qrcode: async (req, res) => {
+        const { text } = req.body;
+
+        const qrCode = qr.image(text, { type: 'png' });
+        res.setHeader("Content-Type", "image/png")
+        qrCode.pipe(res);
+    },
+    imagekitUpload: async(req, res) => {
+        try {
+            // mengubah file menjadi string dengan encoding base64
+            const stringFile = req.file.buffer.toString('base64'); 
+
+            //process upload file ke imagekit
+            const uploadFile = await imagekit.upload({
+                fileName: req.file.originalname,
+                file: stringFile
+            })
+
+            //mengembalikan response ke client
+            return res.status(200).json({
+                status: 'OK',
+                message: 'Success',
+                data: {
+                    name: uploadFile.name,
+                    url: uploadFile.url,
+                    type: uploadFile.fileType
+                }
+            })
+        } catch(err) {
+            throw err;
+        }
     }
 }
--- router/media.js
+++ router/media.js
@@ -2,18 +2,29 @@ const express = require('express');
 const router = express.Router();
 const storage = require('../utils/multer')
 const controller = require('../app/controller')
+const multer = require('multer')();
 
-router.get('/images', express.static('public/images'));
-router.get('/files', express.static('public/files'));
+router.use('/images', express.static('public/images'));
+router.use('/files', express.static('public/files'));
 
 router.post('/api/v2/upload/image', 
         storage.image.single('image'),
         controller.media.uploadImage)
 router.post('/api/v2/upload/video', 
-        storage.image.single('video'),
+        storage.video.single('video'),
         controller.media.uploadVideo)
 router.post('/api/v2/upload/documents', 
-        storage.image.single('doc'),
+        storage.document.single('doc'),
         controller.media.uploadDocument)
+
+//Qr-code
+router.post('/api/v2/qrcode', controller.media.qrcode)
+
+//imagekit
+router.post('/api/v2/upload/imagekit',
+        multer.single('image'),
+        controller.media.imagekitUpload
+)
+
 
 module.exports = router;
