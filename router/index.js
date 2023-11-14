17c66800cd0aa39d4b02f8279b1c130595425f41
Amiruddin Irsyad <amiruddinid@gmail.com>
2023-11-14 21:55:16 +0700

    add hello world to index page

--- router/index.js
+++ router/index.js
@@ -5,6 +5,9 @@ const users = require('./users');
 const auth = require('./auth');
 const media = require('./media');
 
+router.get("/", (req, res) => {
+    res.send("Hello World")
+})
 router.use(users);
 router.use(auth);
 router.use(media);
